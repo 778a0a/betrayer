84741de1c562078fff3efe78649907110e2b793d
778a0a <778a0a@778a0a.com>
2024-05-22 23:26:26 +0900

    ファイル分割

--- Assets/Main/Battle/Battle.cs
+++ Assets/Main/Battle/Battle.cs
@@ -1,120 +1,18 @@
 ﻿using System;
-using System.Buffers;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using UnityEngine;
 using UnityEngine.UIElements;
+using UnityEngine;
 using Random = UnityEngine.Random;
-
-public class BattleManager
-{
-    /// <summary>
-    /// 戦闘を行います。
-    /// </summary>
-    public static async ValueTask<BattleResult> Battle(
-        MapGrid map,
-        Area sourceArea,
-        Area targetArea,
-        Character attacker,
-        Character defender)
-    {
-        var dir = sourceArea.GetDirectionTo(targetArea);
-        var attackerTerrain = map.Helper.GetAttackerTerrain(sourceArea.Position, dir);
-        var defenderTerrain = targetArea.Terrain;
-        Debug.Log($"[戦闘処理] {attacker?.Name}({attacker?.Power}) -> {defender?.Name}({defender?.Power}) at {sourceArea?.Position} -> {targetArea?.Position}");
-        Debug.Log($"[戦闘処理] 攻撃側地形: {attackerTerrain} 防御側地形: {defenderTerrain}");
-        if (defender == null)
-        {
-            Debug.Log($"[戦闘処理] 防御側がいないので侵攻側の勝利です。");
-            return BattleResult.AttackerWin;
-        }
-
-        var world = GameCore.Instance.World;
-        var atk = new CharacterInBattle(attacker, attackerTerrain, world.CountryOf(attacker), true);
-        var def = new CharacterInBattle(defender, defenderTerrain, world.CountryOf(defender), false);
-        atk.Opponent = def;
-        def.Opponent = atk;
-
-        var battle = new Battle(atk, def, world);
-        var result = await battle.Do();
-        return result;
-    }
-
-    /// <summary>
-    /// 戦闘後の回復処理
-    /// </summary>
-    public static void Recover(Character c, bool win)
-    {
-        foreach (var s in c.Force.Soldiers)
-        {
-            if (!s.IsAlive) continue;
-
-            var baseAmount = s.MaxHp * (win ? 0.1f : 0.05f);
-            var adj = Mathf.Max(0, (c.Intelligence - 80) / 100f / 2);
-            var amount = (int)(baseAmount * (1 + adj));
-            s.Hp = Mathf.Min(s.MaxHp, s.Hp + amount);
-        }
-    }
-}
-
-
-public record CharacterInBattle(
-    Character Character,
-    Terrain Terrain,
-    Country Country,
-    bool IsAttacker)
-{
-    public CharacterInBattle Opponent { get; set; }
-    public bool IsDefender => !IsAttacker;
-
-    /// <summary>
-    /// 戦闘の強さ
-    /// 攻撃側ならAttack、防御側ならDefense
-    /// </summary>
-    public int Strength => IsAttacker ? Character.Attack : Character.Defense;
-
-    public Force Force => Character.Force;
-    public bool IsPlayer => Character.IsPlayer;
-    public bool AllSoldiersDead => Character.Force.Soldiers.All(s => !s.IsAlive);
-
-    public static implicit operator Character(CharacterInBattle c) => c.Character;
-
-    public bool ShouldRetreat()
-    {
-        // プレーヤーの場合はUIで判断しているので処理不要。
-        if (IsPlayer) return false;
-
-        // まだ損耗が多くないなら撤退しない。
-        var manyLoss = Character.Force.Soldiers.Count(s => s.Hp < 10) >= 3;
-        if (!manyLoss) return false;
-
-        // 敵よりも兵力が多いなら撤退しない。
-        var myPower = Force.Power;
-        var opPower = Opponent.Force.Power;
-        if (myPower > opPower) return false;
-
-        // 敵に残り数の少ない兵士がいるなら撤退しない。
-        var opAboutToDie = Opponent.Force.Soldiers.Any(s => s.Hp <= 3);
-        if (opAboutToDie) return false;
-
-        // 自国の最後の領土なら撤退しない。
-        var lastArea = Country.Areas.Count == 1;
-        if (lastArea) return false;
-
-        // 撤退する。
-        return true;
-    }
-}
-
 
 public class Battle
 {
     public CharacterInBattle Attacker { get; set; }
     public CharacterInBattle Defender { get; set; }
     private int TickCount { get; set; }
-    
+
     private WorldData World { get; set; }
     private BattleDialog UI => GameCore.Instance.MainUI.BattleDialog;
     private bool NeedUI => Attacker.IsPlayer || Defender.IsPlayer;
@@ -171,7 +69,7 @@ public class Battle
                 BattleResult.AttackerWin;
         }
         Debug.Log($"[戦闘処理] 結果: {result}");
-        
+
         // 画面を更新する。
         if (NeedUI)
         {
---
+++ Assets/Main/Battle/CharacterInBattle.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Random = UnityEngine.Random;
+
+public record CharacterInBattle(
+    Character Character,
+    Terrain Terrain,
+    Country Country,
+    bool IsAttacker)
+{
+    public CharacterInBattle Opponent { get; set; }
+    public bool IsDefender => !IsAttacker;
+
+    /// <summary>
+    /// 戦闘の強さ
+    /// 攻撃側ならAttack、防御側ならDefense
+    /// </summary>
+    public int Strength => IsAttacker ? Character.Attack : Character.Defense;
+
+    public Force Force => Character.Force;
+    public bool IsPlayer => Character.IsPlayer;
+    public bool AllSoldiersDead => Character.Force.Soldiers.All(s => !s.IsAlive);
+
+    public static implicit operator Character(CharacterInBattle c) => c.Character;
+
+    public bool ShouldRetreat()
+    {
+        // プレーヤーの場合はUIで判断しているので処理不要。
+        if (IsPlayer) return false;
+
+        // まだ損耗が多くないなら撤退しない。
+        var manyLoss = Character.Force.Soldiers.Count(s => s.Hp < 10) >= 3;
+        if (!manyLoss) return false;
+
+        // 敵よりも兵力が多いなら撤退しない。
+        var myPower = Force.Power;
+        var opPower = Opponent.Force.Power;
+        if (myPower > opPower) return false;
+
+        // 敵に残り数の少ない兵士がいるなら撤退しない。
+        var opAboutToDie = Opponent.Force.Soldiers.Any(s => s.Hp <= 3);
+        if (opAboutToDie) return false;
+
+        // 自国の最後の領土なら撤退しない。
+        var lastArea = Country.Areas.Count == 1;
+        if (lastArea) return false;
+
+        // 撤退する。
+        return true;
+    }
+}
